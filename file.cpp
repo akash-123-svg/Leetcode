9b1282f218ff04a997f3782ac87c14f6e4d67b3b
Bhagawat Mishra <bhagawatmishra02@gmail.com>
2022-10-08 15:15:46 +0530

    HACKTOBERFEST-ACCEPTED
    HACKTOBERFEST-ACCEPTED

---
+++ file.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+	int n;
+	cin>> n;
+	cout<<n;
+}
